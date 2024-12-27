6af5ea831b44824a7375aedab4ed9c48ca91e193
Ida Yuto <j3312dmf@sit.ac.jp>
2024-12-27 19:23:12 +0900

    自動認識機能を追加

--- train.py
+++ train.py
@@ -52,12 +52,11 @@ learning_rate=1e-3
 optimizer=torch.optim.SGD(model.parameters(),lr=learning_rate)
 
 acc_test=models.test_accuracy(model,dataloader_test)
-print(f'test accuracy:{acc_test*100:.2f}%')
-
-models.train(models,dataloader_test,loss_fn,optimizer)
-
+print(f'test accuracy:{acc_test*100:.3}%')
 acc_test=models.test_accuracy(model,dataloader_test)
 print(f'test accuracy:{acc_test*100:.3f}%')
+
+models.train(models,dataloader_test,loss_fn,optimizer)
 
 n_epochs=5
 
@@ -69,19 +68,29 @@ acc_test_history = []
 for k in range(n_epochs):
     print(f'epoch{k+1}/{n_epochs}')  
     time_start=time.time()
-    loss_train=models.train(model,dataloader_train,loss_fn,optimizer) 
+    loss_train=models.train(model,dataloader_train,loss_fn,optimizer,device=device) 
     time_end=time.time()
     loss_train_history.append(loss_train)
-    print(f'train loss:{loss_train}({time_end-time_start})')
-    loss_test=models.test(models,dataloader_test,loss_fn)
+    print(f'train loss:{loss_train:.3f}({time_end-time_start:.1f}s)',end=',')
+
+    time_start=time.time
+    loss_test=models.test(models,dataloader_test,loss_fn,device=device)
+    time_end=time.time()
     loss_test_history.append(loss_test)
-    print(f'test loss:{loss_test}')
-    acc_train=models.test_accuracy(model,dataloader_train)
-    acc_train_history.append(acc_train)
-    print(f'train accuracy:{acc_train*100:.3f}%')
-    acc_test=model.test_accuracy(model,dataloader_test)
-    acc_test_history.append(acc_test)
-    print(f'test accuracy:{acc_test*100:.3f}){time_end}s)')   
+    print(f'test loss:{loss_test:.3f}({time_end-time_start:.1f}s)')
+
+    if(k+1)%5==0:
+        time_start=time.time()
+        acc_train=models.test_accuracy(model,dataloader_train,device=device)
+        time_end=time.time()
+        acc_train_history.append(acc_train)
+        print(f'train accuracy:{acc_train*100:.3f}%({time_end-time_start:.1f}s)',end=',')
+
+        time_start=time.time()
+        acc_test=model.test_accuracy(model,dataloader_test,device=device)
+        time_end=time.time
+        acc_test_history.append(acc_test)
+        print(f'test accuracy:{acc_test*100:.3f}){time_end-time_start:.1f}s)')   
 
     plt.plot(acc_train_history, label='train')
     plt.plot(acc_test_history, label='test')
